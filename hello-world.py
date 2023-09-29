af9f4c22949fd7a5293c3c1bcf71dc1feb867ea3
dpjavillonar <146396189+dpjavillonar@users.noreply.github.com>
2023-09-29 23:49:57 +0800

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,43 @@
+# Coded by : JAVILLONAR, DANIEL PHILIPPE R.
+# CICTS 1-B
+# Date: SEPTEMBER, 09, 2023
+DividingLine = "_______________________________________________________________"
+Pounds = 133
+print("The given number for Pound weight is  ", Pounds)
+print("The Converted number to Kilograms is ", Pounds/2.205)
+print(DividingLine)
+
+Miles = 200
+print("The given number for Miles is ", Miles)
+print("The converted number for Kilometers is ", Miles*1.609)
+print(DividingLine)
+
+Fahrenheit = 100
+print("The given number for Fahrenheit is ", Fahrenheit)
+print("The converted number to Celsius is ", (Fahrenheit-32)*5/9)
+print(DividingLine)
+
+print("Student 1 is aged 25")
+print("Student 2 is aged 21")
+print("Student 3 is aged 24")
+print("Student 4 is aged 26")
+print("Student 5 is aged 24")
+print("Student 6 is aged 29")
+print("Student 7 is aged 28")
+print("Student 8 is aged 26")
+print("Student 9 is aged 23")
+print("Student 10 is aged 22")
+print('The average age of the students is ', (25+21+24+26+24+29+28+26+23+22)/10)
+print(DividingLine)
+
+Character = ['*Doomstan*', '*Ruffian*', '*Mistu*', '*Moco-Chan*', '*Fuwawa*']
+Equipment = ['*Demon Claws*', '*Peacekeeper*', '*Hell Whip*', '*JugJugJug*', '*Hermes Boots*']
+Abilities = ['*Hyperflex*', '*DemonConfig*', "*Hell's Paradise*", '*Zone*', "*Last Rites*"]
+
+print("At last, at the end of their journey. ", Character[1], "'s party encounters the Dragon emperor, the mythical", Character[2], ".")
+print("With no delay, the emperor uses his ", Equipment[4], "and his", Abilities[3], " ability to rush towards the twin demon dogs " , Character[4], "and", Character[3], ".")
+print(Character[0], " instantly activates ", Abilities[0]," and pulls out his now refined ", Equipment[1], " to blast the emperor on his rapid approach.")
+print(Character[1], "Calls out to the twins to cast their ability ", Abilities[1], "to enhance their weapons", Equipment[0], "for", Character[4], "and", Equipment[2], "for", Character[3])
+print("However, the emperor has a final trick up his sleve,", Abilities[3], "which he activates just in time to survive the party's barrage of attacks.")
+print(Character[1], "Who watched his party struggle against the emperor's desperation decided to play his trump card.", Abilities[2], "which uses up his ")
+print("Entire energy but destorys the area around the emperor to ensure his defeat.", Character[1], 'Focuses, Breathes.... and shout s. "HELLS PARADISE!!!!!!"')
